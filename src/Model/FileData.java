802dfe979d2029a54e35865b04252784cb6b5c87
seanxtr <seanxtr@gmail.com>
2016-05-22 10:07:59 -0700

    add test files

--- src/Model/FileData.java
+++ src/Model/FileData.java
@@ -2,6 +2,11 @@ package Model;
 
 import java.io.*;
 
+/**
+ * Class to store file data
+ * @author Sean
+ *
+ */
 public class FileData implements Serializable {
    private static final long serialVersionUID = 1L;
    private String file_name;
