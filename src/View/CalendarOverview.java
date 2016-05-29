546b76dc69e80ab50352637ef7ecdb22cb43cb40
shuai9532 <shuai9532@gmail.com>
2016-05-29 16:50:37 -0700

    one left

--- src/View/CalendarOverview.java
+++ src/View/CalendarOverview.java
@@ -11,57 +11,57 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 public class CalendarOverview {
-static JFrame frame;
-JFrame CalFrame;
-static ComboBox x;
-static JPanel calendar;
+	static JFrame frame;
+	JFrame CalFrame;
+	static ComboBox x;
+	static JPanel calendar;
 
 	public CalendarOverview(){
-	  
-	   
-	   this.CalFrame= new JFrame("Calendar");
-	   CalFrame.setBackground(Color.WHITE);
-       CalFrame.setLayout(new BorderLayout());
-       
-       // get a box Panel 
-	   x= new ComboBox();  
-	   
-       CalFrame.add(x,BorderLayout.NORTH);
+		
+		this.CalFrame= new JFrame("Calendar");
+		CalFrame.setBackground(Color.WHITE);
+		CalFrame.setLayout(new BorderLayout());
 
-       setCalendar(CalFrame);
-     
+		// get a box Panel 
+		x= new ComboBox();  
+
+		CalFrame.add(x,BorderLayout.NORTH);
+		setCalendar(CalFrame);
+
 	}
-	
+
 	public static void hideWindow(){
 		frame.setVisible(false);
 	}
-	
+
 	public static void ShowWindow(){
 		frame.setVisible(true);
 	}
+	
 	static void setCalendar(JFrame newFrame){
 		// if no year is chosen, then currentYear Panel is null
-    	// then, there will be a nullPointerException
+		// then, there will be a nullPointerException
 		try{ 
-		JFrame Alternative = newFrame;  
-		Alternative.add(x.getCalendarForYear(), BorderLayout.CENTER);
-         Alternative.add(new JPanel(), BorderLayout.SOUTH); 
-         Alternative.setSize(800, 700);
-         Alternative.setResizable(false);
-         Alternative.setVisible(true);       
-	  frame= Alternative;
-		}
-		  catch (Exception e){
-	    	   // there is a nullPointer, 
-	    	   //then we set CurrentYear to the default value
-	    	   JFrame Alternative = newFrame; 
-	    	   Alternative.add(new CalendarOverview_calendar(2016),BorderLayout.CENTER);
-	    	   Alternative.add(new JPanel(), BorderLayout.SOUTH); 
-	           Alternative.setSize(800, 700);
-	           Alternative.setResizable(false);
-	           Alternative.setVisible(true); 
-	    	   frame= Alternative;
-	       }
+			JFrame Alternative = newFrame;  
+			Alternative.add(x.getCalendarForYear(), BorderLayout.CENTER);
+			Alternative.add(new JPanel(), BorderLayout.SOUTH); 
+			Alternative.setSize(800, 700);
+			Alternative.setResizable(false);
+			Alternative.setVisible(true);       
+			frame= Alternative;
+		}	
+		// there is a nullPointer, 
+		//then we set CurrentYear to the default value
+		catch (Exception e){
 	
+			JFrame Alternative = newFrame; 
+			Alternative.add(new CalendarOverview_calendar(2016),BorderLayout.CENTER);
+			Alternative.add(new JPanel(), BorderLayout.SOUTH); 
+			Alternative.setSize(800, 700);
+			Alternative.setResizable(false);
+			Alternative.setVisible(true); 
+			frame= Alternative;
+		}
+
 	}
 }
