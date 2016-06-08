c68d0bedb3649bbfe08fb593d76e3ae7ad6d0652
JiawenZhu <zhujiawen519@gmail.com>
2016-06-07 17:25:45 -0700

    new updates

---
+++ OpenFile.java
@@ -0,0 +1,33 @@
+package Chat;
+
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+/**
+ * opens a coming file
+ * 
+ * @author Jiawen
+ *
+ */
+public class OpenFile {
+	JFileChooser fileChooser = new JFileChooser();
+	public StringBuilder sb = new StringBuilder();
+
+	public void pickAFile() throws Exception {
+		if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			// select a file
+			java.io.File file = fileChooser.getSelectedFile();
+			
+			Scanner input = new Scanner(file);
+
+			while (input.hasNext()) {
+				sb.append(input.nextLine());
+				sb.append("\n");
+			}
+			input.close();
+		} else {
+			sb.append("No File was selected");
+		}
+	}
+
+}
